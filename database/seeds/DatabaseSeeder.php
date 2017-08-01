a9fd8758818405cc29e6c161e0a4f2ef2d9907a1
cemezgn <cemezgn@gmail.com>
2017-08-01 16:25:45 +0300

    ModelFactory and their seeder created.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(App\Company::class, 5)->create();
+        factory(App\Department::class, 25)->create();
+        factory(App\Status::class, 2)->create();
+        factory(App\Employee::class, 25)->create();
+        factory(App\Suggestion::class, 25)->create();
+        factory(App\Mood::class, 25)->create();
+        factory(App\Tag::class, 25)->create();
+        factory(App\MoodTag::class, 25)->create();
     }
 }
