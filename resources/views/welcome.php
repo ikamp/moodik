88f9e710d85223f7c848cadcf1577a2dc6dc736f
İlberk Eral <ilberkeral@gmail.com>
2017-08-02 16:08:18 +0300

    Add homepage
    DSN-501

---
+++ resources/views/welcome.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Moodik</title>
+	<link rel="stylesheet" type="text/css" href="../../public/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../../public/css/home.css">
+	<link rel="stylesheet" type="text/css" href="../../public/css/font-awesome.min.css">
+ </head>
+<body>
+<div class="container-fluid">
+ 	<div class="row topbar">
+		<div class="col-md-2 logo"><b>mood</b>ik</div>
+		<div class="col-md-4"></div>
+		<div class="col-md-6 menu">
+		<ul>
+			<li><a href="">Özellikler <span class="fa fa-angle-down"></span></a><li>
+			<li><a href="">Fiyatlar</a><li>
+			<li><a href="">Destek</a><li>
+			<li><a href="#" class="btn btn-black" role="button">Giriş</a></li>
+			<li><a href="#" class="btn btn-pink" role="button">Ücretsiz Dene!</a></li>
+
+		</ul>
+ 	</div>
+
+	</div>
+	</div>
+	<div class="col-md-2"></div>
+	<div class="col-md-8 info">
+		<h1>Personel Motivasyon Yönetim Aracı!</h1>
+		<p>İzinler, bordro, avans ve harcamalar, zimmetler, özlük bilgileri, yemek kartları ve daha birçok süreci online yönetin. Personel yönetim süreçlerini çalışanlarınızla beraber yürütün.</p>
+       <ul>
+
+			<li><a href="#" class="btn btn-pink" role="button">Ücretsiz Dene!</a></li>
+			<li><a href="#" class="btn btn-white" role="button">Daha Fazla Bilgi</a></li>
+
+
+		</ul>
+	</div>
+	<div class="col-md-2"></div>
+</div>
+
+<script type="text/javascript" src="../../public/js/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="../../public/js/bootstrap.min.js"></script>
+</body>
+</html>
