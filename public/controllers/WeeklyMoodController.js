198454dcc8d9b42506e11b78ee90fa87828e02d2
Serhat Tan <94serhattan@gmail.com>
2017-08-11 12:41:58 +0300

    Updated weekly mood design and navbar bug fixed.

--- public/controllers/WeeklyMoodController.js
+++ public/controllers/WeeklyMoodController.js
@@ -2,7 +2,7 @@ angular.module('moodikApp')
     .controller('WeeklyMoodController', weeklyMoodController);
 
 function weeklyMoodController($scope, $rootScope, DataService, $location, $routeParams) {
-
+    $rootScope.flag = false;
     $scope.weeklyMood = function (weekMood) {
         weekMood.employeeId = $rootScope.user.id;
         weekMood.week = $routeParams.id;
